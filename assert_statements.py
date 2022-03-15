451759eac6336ddf4eb668eeb0b6a9ebb8b59cfa
Zaier <veraalber989@gmail.com>
2022-03-15 16:57:28 -0300

    Probando 'assert statements' algo que no es tan recurrente en el manejo de errores, pero que sin duda se puede combinar con los clasicos

---
+++ assert_statements.py
@@ -0,0 +1,16 @@
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+def run():
+    num = input("Ingresa un numero: ")
+    assert num.isnumeric(), "Por favor ingrese un numero"
+    print(divisors(int(num)))
+    print("Termino el programa")
+
+
+if __name__ == '__main__':
+    run()
